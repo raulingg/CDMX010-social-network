cae4cbd044f1d4adfe71d05cbc6ca672795fd34e
Raul Quispe <relaxedchild@gmail.com>
2021-01-29 18:25:58 -0500

    refactor: wrap firebase instance

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,28 +1,27 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAxKjqTudxBWi9j9i4IDKn450hmM1m_w3A",
-    authDomain: "red-social-coaches.firebaseapp.com",
-    projectId: "red-social-coaches",
-    storageBucket: "red-social-coaches.appspot.com",
-    messagingSenderId: "489293961166",
-    appId: "1:489293961166:web:e0ae10e7b6db857d26fd9f"
-};
-firebase.initializeApp(firebaseConfig);
-
-let database = firebase.firestore();
-export let auth = firebase.auth();
-
-export const createPosts = ({message, user}) => {
-    database.collection("posts").add({
+export default (firebase) => {
+  const createPost = ({ message, user }) => firebase.firestore()
+    .collection('posts')
+    .add({
       message,
-      user
+      user,
     })
-    .then(docRef => console.log("Document written with ID: ", docRef.id))
-    .catch(error => console.error("Error adding document: ", error));
-};
+    .then((docRef) => console.log('Document written with ID: ', docRef.id))
+    .catch((error) => console.error('Error adding document: ', error));
 
-export const login = (email, password) => {
-  return firebase.auth().signInWithEmailAndPassword(email, password)
-  .then((user) => console.log('sign in', user))
-  .catch((error) => console.log('error', error)); 
-};
+  const signIn = (email, password) =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    firebase.auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((user) => console.log('user has signed in!', user));
 
+  const signOut = () => firebase.auth().signOut();
+
+  const getUser = () => firebase.auth().currentUser;
+
+  return {
+    createPost,
+    signIn,
+    signOut,
+    getUser,
+  };
+};
