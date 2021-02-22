6692b2fcef961eba277253f11c95ad0381f475c6
AnaliMtzart <analimjack@gmail.com>
2021-02-22 12:53:05 -0600

    click en signUp

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
+/* eslint-disable quotes */
 // Este es el punto de entrada de tu aplicacion
 // import { myFunction } from './lib/index.js';
 // myFunction();
 
-import { routes, onNavigate } from './routes.js';
+import { routes, onNavigate } from "./routes.js";
 
 let rootDiv = null;
-
-window.addEventListener('DOMContentLoaded', () => {
-  rootDiv = document.getElementById('root');
-  rootDiv.innerHTML = routes[window.location.pathname];
-  document.querySelector('#login').addEventListener('click', () => {
-    const navigate = onNavigate('/signUp');
+function agregarEvento() {
+  document.querySelector("#login").addEventListener("click", () => {
+    const navigate = onNavigate("/signUp");
     rootDiv.innerHTML = navigate;
-  }); // eventodeboton
-});
+  });
+}
 
-//document.querySelector('#gmailIcon').addEventListener('click', authGoogle);
+window.addEventListener("DOMContentLoaded", () => {
+  rootDiv = document.getElementById("root");
+  rootDiv.innerHTML = routes[window.location.pathname];
+  agregarEvento();
+  // eventodeboton
+});
 
 window.onpopstate = () => {
   rootDiv.innerHTML = routes[window.location.pathname];
+  agregarEvento();
 };
