25f12a106ef534be6710068e9936bd4f0927b635
Denisse <ledennii@gmail.com>
2021-03-03 09:22:11 -0600

    y

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,30 @@
-export const toViewHome = `
-<div class="allContainer">
- <div class="section">  
-   <button type="button" class="btnType" id="toInit">Iniciar sesión</button>
-   <button type="button" class="btnType" id="toCreate">Crear cuenta</button>
-  </form>
- </div>
-</div>
-`;
-/* <h3>I am Home</h3> */
+import {toViewLogIn} from './login.js';
+import {toViewSingUp} from './singup.js';
+export const toViewHome = 
+  `
+  <div class="allContainer">
+   <div class="section">  
+     <button type="button" class="btnType" id="toInit">Iniciar sesión</button>
+     <button type="button" class="btnType" id="toCreate">Crear cuenta</button>
+    </form>
+   </div>
+  </div>
+  `;
+window.addEventListener('DOMContentLoaded', () => {
+  const toInitB = document.getElementById("toInit") 
+  toInitB.addEventListener('click', (e) => {
+   e.preventDefault();
+  //  console.log("entro a boton inicio");  
+  root.innerHTML = "";
+  root.innerHTML = toViewLogIn;
+   })
+});
+window.addEventListener('DOMContentLoaded', () => {
+  const toCreateB = document.getElementById("toCreate") 
+  toCreateB.addEventListener('click', (e) => {
+   e.preventDefault();
+  //  console.log("entro a boton inicio");  
+  root.innerHTML = "";
+  root.innerHTML = toViewSingUp;
+   })
+});
