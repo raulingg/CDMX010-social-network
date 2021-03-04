095227c2ca8d1dc076af474da499ae6ec4b4edc8
Raul Robinson Quispe Mendez <9435850+raulingg@users.noreply.github.com>
2021-03-04 02:20:21 +0000

    refactor(posting): rehouse logic.

--- src/posting.js
+++ src/posting.js
@@ -1,17 +1,52 @@
-export const posting = () => {
-  const rootDiv = document.getElementById('root');
-  const htmlForm = `
+import { savePost, updatePost, getPostById } from './lib/firebase.js';
+
+const getSearchParam = (param) => (new URLSearchParams(window.location.search)).get(param);
+
+export const posting = async (target) => {
+  const html = `
   <div id="post-form">
-  <form id="form">
-    <h1>Escribe tu post</h1>
-    <input id="post-title" type="text" placeholder="Título" required>
-    <input id="post-location" type="text" placeholder="Coordenadas" required>
-    <textarea id="post-description" type="text" placeholder="Descripción" required minlength="20" rows="8"></textarea>
-    <button id="save" value="submit">Publicar</button>
-  </form>  
-</div>
-    `;
-  rootDiv.innerHTML = htmlForm;
+    <form id="form">
+      <h1>Escribe tu post</h1>
+      <input id="title" type="text" placeholder="Título" required>
+      <input id="location" type="text" placeholder="Coordenadas" required>
+      <textarea id="description" type="text" placeholder="Descripción" required minlength="20" rows="8"></textarea>
+      <button id="save" value="submit">Publicar</button>
+    </form>  
+  </div>
+  `;
+  target.innerHTML = html;
+
+  const postId = getSearchParam('id')
+  const postForm = document.getElementById('form');
+  postForm.title.focus();
+  let post = null
+
+  if (postId) {
+    post = await getPostById(postId);
+    postForm.title.value = post.title;
+    postForm.location.value = post.location;
+    postForm.description.value = post.description;
+    postForm.save.innerText = 'Actualizar'; 
+  }
+
+  postForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const postData = {
+      title: postForm.title.value,
+      location: postForm.location.value,
+      description: postForm.description.value,
+      likes: post && post.likes ? post.likes : 0,
+    };
+
+    if (post) {
+      await updatePost(postId, postData);
+      postForm.save.innerText = 'Guardar';
+    } else {
+      await savePost(postData);
+      // limpia o resetea el formulario
+      postForm.reset();
+    }
+  });
 };
 
 export default posting;
