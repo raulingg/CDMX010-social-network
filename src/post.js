dc8e791f9cae21736b0129f4376d95afc9afd0cb
Raul Robinson Quispe Mendez <9435850+raulingg@users.noreply.github.com>
2021-03-04 02:20:09 +0000

    refactor(post): rehouse logic.

--- src/post.js
+++ src/post.js
@@ -1,11 +1,41 @@
-// import CardPost from './components/CardPost';
+import CardPost from './components/CardPost.js';
+import { deletePost, getAllPosts } from './lib/firebase.js';
+import { onNavigate } from './routes.js';
 
-export const post = (target) => {
-  // const postContainer = document.getElementById('post-container');
+export const post = async (target) => {
   const templeteHome = `
     <div id="post-container"></div>
   `;
   target.innerHTML = templeteHome;
-  // postContainer.innerHTML = CardPost;
+  
+  const posts = await getAllPosts()
+  
+  // render posts
+  const postTemplates = posts.map(post => CardPost(post));
+  const postContainer = document.getElementById('post-container');
+  postContainer.innerHTML = postTemplates.join('');
+  
+  // register/attach edit events
+  const btnsEdit = document.querySelectorAll('.btn-edit');
+  btnsEdit.forEach(btn => {
+    btn.addEventListener('click', async (e) => {
+      e.preventDefault();
+      
+      const url = new URL(e.currentTarget.href)
+      onNavigate(url.pathname + url.search);   
+    });
+  });
+  
+  // register/attach delete events
+  const btnsDelete = document.querySelectorAll('.btn-delete');
+  btnsDelete.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      const confirmar = confirm('¿Seguro que quieres borrar tu post?');
+      if (confirmar) {
+        deletePost(e.target.dataset.id);
+      }
+    });
+  });
 };
+
 export default post;
