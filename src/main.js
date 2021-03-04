9a56eee7885d4b3bd33cf66cdad3a167f99b770f
Raul Robinson Quispe Mendez <9435850+raulingg@users.noreply.github.com>
2021-03-04 02:23:40 +0000

    refactor: move view logic to respective files

--- src/main.js
+++ src/main.js
@@ -1,127 +1,24 @@
 // En este documento solamente tenemos lo que manipula el DOM
-import { routes, onNavigate } from './routes.js';
+import { onNavigate } from './routes.js';
 // Importamos navigate para manipular el DOM con los botones del footer
-import { dataBase, savePost, getPostInfo, onGetPost, deletePost, updatePost } from './lib/firebase.js';
-import { posting } from './posting.js';
-import { home } from './home.js';
-import { post } from './post.js';
-import CardPost from './components/CardPost.js';
+import { getPostById } from './lib/firebase.js';
 
-posting();
-const rootDiv = document.getElementById('root');
 const postForm = document.getElementById('form');
 
-// vista al cargar home
-window.addEventListener('DOMContentLoaded', () => {
-  const home = routes[window.location.pathname];
-  home(rootDiv);
-  onGetPost((querySnapshot) => {
-    const postList = document.getElementById('post-list');
-    postList.innerHTML = '';
-    querySnapshot.forEach((doc) => {
-    // contante para llamar la data del post
-      const post = doc.data();
-      // llama al id del post
-      post.id = doc.id;
-      // console.log(post);
-      postList.innerHTML += `
-        <a href="#" class="single-post"><h2 class="title-list">${post.title}</h2></a>
-        `;
-    });
-  });
-});
-
-// myFunction();
 // Funciones para botones de navegación
-document.getElementById('toHome').addEventListener('click', async (e) => {
+document.getElementById('toHome').addEventListener('click', (e) => {
   e.preventDefault();
   onNavigate('/');
-  home(rootDiv);
-  onGetPost((querySnapshot) => {
-    const postList = document.getElementById('post-list');
-    postList.innerHTML = '';
-    querySnapshot.forEach((doc) => {
-    // contante para llamar la data del post
-      const post = doc.data();
-      // llama al id del post
-      post.id = doc.id;
-      // console.log(post);
-      postList.innerHTML += `
-      <a href="#" class="single-post" data-id="${post.id}"><h2 class="title-list">${post.title}</h2></a>
-        `;
-      const btnsPost = document.querySelectorAll('.single-post');
-      btnsPost.forEach((btn) => {
-        btn.addEventListener('click', async (e) => {
-          e.preventDefault();
-          onNavigate('/singlepost');
-          const doc = await getPostInfo(e.target.dataset.id);
-          console.log(doc);
-        });
-      });
-    });
-  });
 });
-const toForm = document.getElementById('toForm');
-toForm.addEventListener('click', async (e) => {
+
+document.getElementById('toPosting').addEventListener('click', async (e) => {
   e.preventDefault();
   onNavigate('/posting');
-  posting();
-  let editStatus = false;
-  const postForm = document.getElementById('form');
-  postForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const title = postForm['post-title'];
-    const location = postForm['post-location'];
-    const description = postForm['post-description'];
-    let likes = '0';
-    let id = '';
-    if (!editStatus) {
-      await savePost(title.value, location.value, description.value, likes);
-    } else {
-      await updatePost(id, {
-        title: title.value,
-        location: location.value,
-        description: description.value,
-        likes: likes.value,
-      });
-      editStatus = false;
-      postForm.save.innerText = 'Guardar';
-      id = '';
-    }
-    // limpia o resetea el formulario
-    postForm.reset();
-    title.focus();
-    console.log(title, location, description);
-    e.preventDefault();
-  });
 });
+
 document.getElementById('toPost').addEventListener('click', (e) => {
   e.preventDefault();
   onNavigate('/post');
-  post(rootDiv);
-  onGetPost((querySnapshot) => {
-    const postContainer = document.getElementById('post-container');
-    postContainer.innerHTML = '';
-    querySnapshot.forEach((doc) => {
-    // contante para llamar la data del post
-      const post = doc.data();
-      // llama al id del post
-      post.id = doc.id;
-      console.log(post.id);
-      // console.log(post);
-      postContainer.innerHTML += CardPost(post);
-      editP();
-      const btnsDelete = document.querySelectorAll('.btn-delete');
-      btnsDelete.forEach(btn => {
-        btn.addEventListener('click', async (e) => {
-          const confirmar = confirm('¿Seguro que quieres borrar tu post?');
-          if (confirmar === true) {
-            await deletePost(e.target.dataset.id);
-          }
-        });
-      });
-    });
-  });
 });
 // const btnsDelete = document.querySelectorAll('.btn-delete');
 // console.log(btnsDelete);
@@ -204,30 +101,11 @@ btnsDelete.forEach(btn => {
                 await deletePost(e.target.dataset.id);
             });
         }); */
-function editP() {
-  const btnsEdit = document.querySelectorAll('.btn-edit');
-  btnsEdit.forEach(btn => {
-    btn.addEventListener('click', async (e) => {
-      e.preventDefault();
-      onNavigate('/posting');
-      const postForm = document.getElementById('form');
-      const doc = await getPostInfo(e.target.dataset.id);
-      const postEditing = doc.data();
-      editStatus = true;
-      id = doc.id;
-      postForm['post-title'].value = postEditing.title;
-      postForm['post-location'].value = postEditing.location;
-      postForm['post-description'].value = postEditing.description;
-      postForm['save'].innerText = 'Actualizar';    
-    });
-  });
-}
 // Botón de editar
 const btnsEdit = document.querySelectorAll('.btn-edit');
 btnsEdit.forEach(btn => {
   btn.addEventListener('click', async (e) => {
-    const doc = await getPostInfo(e.target.dataset.id);
-    const postEditing = doc.data();
+    const postEditing =  await getPostInfo(e.target.dataset.id);
     editStatus = true;
     id = doc.id;
     postForm['post-title'].value = postEditing.title;
