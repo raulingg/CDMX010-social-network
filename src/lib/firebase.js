1aacdaa58e40707d4b3ba4e4540f536df332d55a
Raul Robinson Quispe Mendez <9435850+raulingg@users.noreply.github.com>
2021-03-04 02:18:40 +0000

    refactor(firebase): add getAllPosts function.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,7 @@
 // llamar a Firestore
 export const dataBase = firebase.firestore();
 
-export const savePost = (title, location, description, likes) =>
+export const savePost = ({ title, location, description, likes }) =>
   dataBase.collection('post').doc().set({
     title,
     location,
@@ -10,11 +10,27 @@ export const savePost = (title, location, description, likes) =>
   });
 
 // de la colección quiero un documento con el id que se obtiene al dar click
-export const getPostInfo = (id) => dataBase.collection('post').doc(id).get();
+export const getAllPosts = async (id) => {
+  const querySnapshot = await dataBase.collection('post').get();
+  const posts = [];
+  querySnapshot.forEach((doc) => {
+      const post = doc.data();
+      post.id = doc.id;
+      posts.push(post);      
+  });
+
+  return posts
+};
+
+export const getPostById = async (id) => {
+  const doc = await  dataBase.collection('post').doc(id).get();
+
+  return doc.data();
+}
+
 export const onGetPost = (callback) => dataBase.collection('post').onSnapshot(callback);
 // para eliminar un post da un parametro id
 export const deletePost = (id) => dataBase.collection('post').doc(id).delete();
 // función actuaalizar en firebase
 
-export const updatePost = (id, updatedPost) =>
-dataBase.collection('post').doc(id).update(updatedPost);
+export const updatePost = (id, updatedPost) => dataBase.collection('post').doc(id).update(updatedPost);
